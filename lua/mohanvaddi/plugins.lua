d4e5e2c00775ac7b987441a5fb959eb5a6152bb2
Mohan Vaddi <myselfgeek01@gmail.com>
2022-12-10 15:37:10 +0530

    feat: 🎸 added comments
    -- can add single line or multi-line comments using "gc"

--- lua/mohanvaddi/plugins.lua
+++ lua/mohanvaddi/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "numToStr/Comment.nvim" -- Easily comment stuff
 
   -- colorscheme
   use "LunarVim/darkplus.nvim"
@@ -79,6 +80,7 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
   use "p00f/nvim-ts-rainbow"   -- for bracket-pair colorizer
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
