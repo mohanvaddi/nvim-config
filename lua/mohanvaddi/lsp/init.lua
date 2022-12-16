8a040d828902440b9b54e3eae6b489844d0fb101
Mohan Vaddi <myselfgeek01@gmail.com>
2022-12-17 00:15:41 +0530

    feat: 🎸 a simple config (theprimeagen)

--- lua/mohanvaddi/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-
-require "mohanvaddi.lsp.mason"
-require("mohanvaddi.lsp.handlers").setup()
-require "mohanvaddi.lsp.null-ls"
---
+++ lua/user/plugins.lua
@@ -0,0 +1,18 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require 'packer'.startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		requires = { { 'nvim-lua/plenary.nvim' } } }
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use('theprimeagen/harpoon')
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+end)
